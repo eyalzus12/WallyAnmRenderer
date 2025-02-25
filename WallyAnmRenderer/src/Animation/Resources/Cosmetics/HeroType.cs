454779e37619a13448dca7c09e587d7f6abd6a61
EyalZusiman <eyal.zusiman@gmail.com>
2025-02-25 22:26:29 +0200

    remove unused imports

--- WallyAnmRenderer/src/Animation/Resources/Cosmetics/HeroType.cs
+++ WallyAnmRenderer/src/Animation/Resources/Cosmetics/HeroType.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Xml.Linq;
 using System.Globalization;
-using BrawlhallaAnimLib.Gfx;
-using BrawlhallaAnimLib.Reading;
 
 namespace WallyAnmRenderer;
 
