25a16b297e93620946d9b7eaa0f593a33de8be25
Talafhah1 <omar.talafhah1@gmail.com>
2025-03-01 15:55:12 +0300

    putting random at the end

--- WallyAnmRenderer/src/Animation/Resources/Cosmetics/CostumeTypes.cs
+++ WallyAnmRenderer/src/Animation/Resources/Cosmetics/CostumeTypes.cs
@@ -33,10 +33,11 @@ public readonly struct CostumeTypes : ICsvReader
             _gfx[costumeName] = info;
         }
 
-        _infos = _infos.OrderBy(x => heroTypes.TryGetHero(x.Value.OwnerHero, out HeroType? hero) ? hero.ReleaseOrderId : 0)
-                .ThenBy(x => x.Value.CostumeIndex)
-                .ThenBy(x => x.Value.CostumeName)
-                .ToDictionary(x => x.Key, x => x.Value);
+        _infos = _infos.OrderBy(x => x.Value.OwnerHero == "Random" ? int.MaxValue : 0)
+                        .ThenBy(x => heroTypes.TryGetHero(x.Value.OwnerHero, out HeroType? hero) ? hero.ReleaseOrderId : 0)
+                        .ThenBy(x => x.Value.CostumeIndex)
+                        .ThenBy(x => x.Value.CostumeName)
+                        .ToDictionary(x => x.Key, x => x.Value);
 
         Dictionary<string, CostumeTypesGfx> sortedGfx = [];
         foreach (string key in _infos.Keys)
